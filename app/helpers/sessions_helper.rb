6fcc8233f04e3b0bf0f64b33635f1bc400d25e4e
ahza64 <ahza64@yahoo.com>
2016-05-12 23:13:52 -0700

    walk through Auth, no flash, no refactor, no checking or testing

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,22 @@
 module SessionsHelper
+
+  def login(user)
+    session[:user_id]
+    @current_user = user
+  end
+
+  def current_user
+    @current_user ||= User.find(sessions[:user_id])
+  end
+
+  def logged_in?
+    if current_user == nil
+      redirect_to new_sessions_path
+    end
+  end
+
+  def logout
+    @current_user = session[:user_id] = nil
+  end
+
 end
