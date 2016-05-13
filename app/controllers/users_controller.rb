934693e09f4448661dda01f85867d5f7121b9b62
ahza64 <ahza64@yahoo.com>
2016-05-12 22:51:54 -0700

    user set up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,19 @@
 class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def create
+    user_params = params.require(:user).permit(:username, :email, :password, :location)
+    @user = User.create(user_params)
+    redirect_to user_path(@user)
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render :show
+  end
+
 end
