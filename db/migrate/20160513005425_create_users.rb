b240ffaa49fed409941a8e9be70721fafc1910fc
ahza64 <ahza64@yahoo.com>
2016-05-12 22:34:25 -0700

    alter user migration file before migration

--- db/migrate/20160513005425_create_users.rb
+++ db/migrate/20160513005425_create_users.rb
@@ -4,7 +4,7 @@ class CreateUsers < ActiveRecord::Migration
       t.string :username
       t.string :email
       t.string :location
-      t.string :password
+      t.string :password_digest
       t.string :image
 
       t.timestamps null: false
