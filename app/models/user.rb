8217cd22aa82125d8a791d923564226d4d3894cc
ruhsam <ruhsam@gmail.com>
2016-05-15 16:55:40 -0700

    validations on email, username, completed. Need to add password length

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ActiveRecord::Base
+  validates :username, presence: true
+  validates :location, presence: true
+  VALID_EMAIL_REGEX = /\w+@[^\.]+\.\w+/
+  validates :email,
+            presence:   true,
+            format:     { with: VALID_EMAIL_REGEX },
+            uniqueness: true,
+            length:     { maximum: 255 }
+  validates :password_digest, presence: true, length: { minimum: 8 }
+
   has_secure_password
   has_many :tasks
 
@@ -6,5 +16,5 @@ class User < ActiveRecord::Base
     @user = User.find_by({email: params[:email]})
     @user.try(:authenticate, params[:password])
   end
-    
+
 end
