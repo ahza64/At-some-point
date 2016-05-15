cdb306b30b8207daf7e4a0d81376168609274481
ruhsam <ruhsam@gmail.com>
2016-05-15 13:43:28 -0700

    commit did not take, second commit for rspec tests in sessions controller

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -2,31 +2,33 @@ require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
    describe "#new" do
-       it "saves a new user" do
+      it "saves a new user" do
          # item = Item.create({size:'s', color:'blue', status:'unsold'})
          get :new, id: Session.id
          expect(response).to render_template(:new)
-       end
-     end
+      end
+   end
 
    describe "#create" do
       it "verifies the user information and logs in user"
    end
-      it "redirects to user profile if login is successful"
-   end
-      it "redirects to home page if login is not successful"
-   end
+   it "redirects to user profile if login is successful"
+end
+it "redirects to home page if login is not successful"
+end
 
-   describe "#destroy" do
-      it "logs user out, ends user session"
-   end
+describe "#destroy" do
+   it "logs user out, ends user session"
+end
+it "redirects to home page when logged out"
+end
 
-   describe "#testing" do
-       it "tests rspec setup in controller" do
-         # item = Item.create({size:'s', color:'blue', status:'unsold'})
-         # get :testing, id: item.id
-         expect(session.testing?).to be true
-       end
-    end
+describe "#testing" do
+   it "tests rspec setup in controller" do
+      # item = Item.create({size:'s', color:'blue', status:'unsold'})
+      # get :testing, id: item.id
+      expect(session.testing?).to be true
+   end
+end
 
 end
