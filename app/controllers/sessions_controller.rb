c530c0b64dc1417884bfe3dd4c9e7250a13d521e
ruhsam <ruhsam@gmail.com>
2016-05-15 11:51:47 -0700

    branch merge issues corrected

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,22 @@
 class SessionsController < ApplicationController
+   def new
+     @user = User.new
+     render :new
+   end
+
+   def create
+     user_params = params.require(:user).permit(:email, :password)
+     @user = User.confirm(user_params)
+     if @user
+       login(@user)
+       redirect_to @user
+     else
+       redirect_to home_path
+     end
+   end
+
+   def destroy    #destroy user login
+     logout   #see in sessions helper
+     redirect_to home_path
+   end
 end
