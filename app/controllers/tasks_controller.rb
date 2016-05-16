4727c36f3c38bfe93f7578d55c211f3be09022f2
Jonathan <jortiz@tunein.com>
2016-05-16 15:58:40 -0700

    need to pull for playgorund

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,57 @@
 class TasksController < ApplicationController
+  def new
+    @task = Task.new
+    @user = User.find(params[:user_id])
+    if current_user
+      render :new
+    else
+      #flash see john
+      redirect_to home_path #change to login page when we make it see jon
+    end
+  end
+
+  def create
+    @task = Task.create(task_params)
+    @user = current_user
+    @user.task.unshift(@task)
+    redirect_to user_path(@user)
+  end
+
+  def edit
+    @task = Task.find(params[:task_id])
+    @user = @task.user
+    if current_user == @user
+      render :edit
+    else
+      # flash see john
+      redirect_to home_path
+    end
+  end
+
+  def update
+    @task = Task.find(params[:task_id])
+    @task.update(task_params)
+    #flash see john
+    redirect_to @user
+  end
+
+  def destroy
+    @task = Task.find(params[:task_id])
+    @user = @task.user
+    if current_user == @user
+      @user.task.destroy(@task)
+      #flash see john
+      redirect_to @user
+    else
+      #flash see john
+      redirect_to home_path
+    end
+  end
+
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :location)
+  end
 end
--- config/routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-  root "home#index", as: "home"
-  resources :tasks, :users, :sessions
-
-  get "/login", to: "sessions#new"
-  get "/logout", to: "sessions#destroy"
-
-end
